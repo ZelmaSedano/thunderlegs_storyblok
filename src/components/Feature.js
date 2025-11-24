cb91ec67add2f4a9021277a53978fd46be601070
ZelmaSedano <zvsedano@gmail.com>
2025-11-24 08:27:07 -0600

    playing with div structure & adding backgroundColor property

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -2,7 +2,10 @@ import { storyblokEditable } from "@storyblok/react";
 import './Feature.css'
 
 const Feature = ({ blok }) => (
-  <div {...storyblokEditable(blok)} className="column feature">
+  <div {...storyblokEditable(blok)} 
+  className="column feature"
+  style={{ backgroundColor: blok.backgroundColor}}
+  >
     {blok.name}
   </div>
 );
